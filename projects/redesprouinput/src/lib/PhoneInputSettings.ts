1ed77ba4c9fd19048f7b3825fc38e1d7c6501b69
Erivan <erivan.nas@gmail.com>
2019-07-02 15:33:46 -0300

    Adiciona classe phone input

---
+++ projects/redesprouinput/src/lib/PhoneInputSettings.ts
@@ -0,0 +1,14 @@
+export class PhoneInputSettings {
+    disabled: boolean = false;
+    defaultCountryFlag: string = 'br';
+    defaultCountryCode: string = '+55';
+    translations = {
+        'Brazil': 'Brasil',
+        'United States': 'Estados Unidos',
+    }
+    
+    constructor() { }
+
+    
+}
+
